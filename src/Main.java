b0794869c5e6ad60b3d461ac1a7c22f45bafd409
CatStorm-dev <wapitiblase36@gmail.com>
2023-02-18 15:27:11 +0100

    Added Data

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,10 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Random;
+import java.util.Scanner;
 
 /* TODOs
  * TODO Make the init function pull from files
@@ -11,26 +17,26 @@ public class Main
     public static HiddenNeuron[] Layer2 = new HiddenNeuron[5];
     public static OutNeuron[] Layer3 = new OutNeuron[3];
 
-    public static void main(String[] args)
+    public static void main(String[] args) throws IOException
     {
-        run();
+        System.out.println(Files.readString(Paths.get("")));
+        //init();
+        //run();
     }
 
-    public static void init_old()
+    public static void init()
     {
         for (int n = 0; n < Layer3.length; n++)
         {
             Layer3[n] = new OutNeuron();
-            Layer3[n].bias = (short) ((short) new Random().nextInt(128-(-127)) - 128);
+            //Layer3[n].bias =
         }
 
         for (int n = 0; n < Layer2.length; n++)
         {
             Layer2[n] = new HiddenNeuron();
-            Layer2[n].bias = (short) ((short) new Random().nextInt(128-(-127)) - 128);
             for (int m = 0; m < Layer2[n].links.length; m++)
             {
-                Layer2[n].links[m].weight = (short) ((short) new Random().nextInt(128-(-127)) - 128);
                 Layer2[n].links[m].out = Layer3[m];
             }
         }
@@ -38,11 +44,8 @@ public class Main
         for (int n = 0; n < Layer1.length; n++)
         {
             Layer1[n] = new InNeuron();
-            Layer1[n].bias = 100; //(short) ((short) new Random().nextInt(128-(-127)) - 128);
-            Layer1[n].value = (short) ((short) new Random().nextInt(128-(-127)) - 128);
             for (int m = 0; m < Layer1[n].links.length; m++)
             {
-                Layer1[n].links[m].weight = (short) ((short) new Random().nextInt(128-(-127)) - 128);
                 Layer1[n].links[m].out = Layer2[m];
             }
         }
