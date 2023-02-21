4cfca796e196f5bbe54b77f5f8cecf6a9169dd6f
CatStorm-dev <wapitiblase36@gmail.com>
2023-02-21 15:30:26 +0100

    Made init method pull from Data and initialize the Neurons with that Data

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,8 @@
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Random;
 import java.util.Scanner;
 
 /* TODOs
- * TODO Make the init function pull from files
  * TODO Training class
 */
 
@@ -19,34 +14,40 @@ public class Main
 
     public static void main(String[] args) throws IOException
     {
-        System.out.println(Files.readString(Paths.get("")));
-        //init();
-        //run();
+        init();
+        run();
     }
 
-    public static void init()
+    public static void init() throws IOException, NullPointerException
     {
         for (int n = 0; n < Layer3.length; n++)
         {
             Layer3[n] = new OutNeuron();
-            //Layer3[n].bias =
+            Layer3[n].bias = new Scanner(new File("src/Data/Layer3/Bias/_" + n).toURL().openStream()).nextShort();
         }
 
         for (int n = 0; n < Layer2.length; n++)
         {
             Layer2[n] = new HiddenNeuron();
+            Layer2[n].bias = new Scanner(new File("src/Data/Layer2/Bias/_" + n).toURL().openStream()).nextShort();
+
             for (int m = 0; m < Layer2[n].links.length; m++)
             {
                 Layer2[n].links[m].out = Layer3[m];
+                Layer2[n].links[m].weight = new Scanner(new File("src/Data/Layer2/Links/_" + n + "/_" + m).toURL().openStream()).nextShort();
             }
         }
 
         for (int n = 0; n < Layer1.length; n++)
         {
             Layer1[n] = new InNeuron();
+            Layer1[n].bias = new Scanner(new File("src/Data/Layer1/Bias/_" + n).toURL().openStream()).nextShort();
+            Layer1[n].value = new Scanner(new File("src/Data/Layer1/Value/_" + n).toURL().openStream()).nextShort();
+
             for (int m = 0; m < Layer1[n].links.length; m++)
             {
                 Layer1[n].links[m].out = Layer2[m];
+                Layer1[n].links[m].weight = new Scanner(new File("src/Data/Layer1/Links/_" + n + "/_" + m).toURL().openStream()).nextShort();
             }
         }
     }
